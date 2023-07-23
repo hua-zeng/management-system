dceaf02bdfee77ff9903e919810fb2614d4a2c39
Hua Zeng <huazeng@catherines-air.myfiosgateway.com>
2023-07-23 18:10:41 -0400

    added changes to styles

--- management-system/src/main.tsx
+++ management-system/src/main.tsx
@@ -1,5 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+// 样式初始化置顶
+import "reset-css"
+// UI框架样式
+
+// 全局样式
+import "@/assets/styles/global.scss"
+// 组件样式
 import App from './App.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
